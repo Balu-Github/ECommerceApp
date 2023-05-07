51a5a5dfb37cd6e7c823a7a41708d86751b97a4f
Balakrishna Barre <balakrishna.b3@outlook.com>
2023-05-07 11:00:20 +0530

    reverted changes

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Heading = ({ text1 = "Our", text2 = "Products Test", containerStyle }) => {
+const Heading = ({ text1 = "Our", text2 = "Products", containerStyle }) => {
   return (
     <View style={containerStyle}>
       <Text style={{ fontSize: 25 }}>{text1}</Text>
